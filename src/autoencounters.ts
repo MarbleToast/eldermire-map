dd25de1ff301d84ea3225bbfce4989b896bfb545
MarbleToast <jrbrad44@hotmail.co.uk>
2025-05-08 13:50:28 +0200

    Wallowbank

--- src/autoencounters.ts
+++ src/autoencounters.ts
@@ -1,131 +1,131 @@
 const autoencounters: Record<number, string> = {
-    // === THE KNOTS ===
-    4182: "Miremal Path and Wounded Serpopard",
-    4139: "Miremal Path",
-    4055: "Miremal Path",
-    4014: "Miremal Path",
-    4056: "Miremal Path",
-    4140: "Miremal Path",
+	// === THE KNOTS ===
+	4182: "Miremal Path and Wounded Serpopard",
+	4139: "Miremal Path",
+	4055: "Miremal Path",
+	4014: "Miremal Path",
+	4056: "Miremal Path",
+	4140: "Miremal Path",
 
-    3597: "Spiderlings Bridge",
+	3597: "Spiderlings Bridge",
 
-    4434: "Livestock's Blood Trail",
+	4434: "Livestock's Blood Trail",
 
-    3721: "Twisting Vines with Brain Hoods",
-    3679: "Twisting Vines",
-    3680: "Twisting Vines",
-3847: "Chest in Middle of Pool",
-4013: "Shrine to Kelemvor",
+	3721: "Twisting Vines with Brain Hoods",
+	3679: "Twisting Vines",
+	3680: "Twisting Vines",
+	3847: "Chest in Middle of Pool",
+	4013: "Shrine to Kelemvor",
 
-    3549: "Spirit Elf Battalion (Night)",
-    3508: "Spirit Elf Battalion (Night)",
-    3592: "Spirit Elf Battalion (Night)",
-    4435: "Blood Trail towards Wolf Cave", //"Moonweaver in a Bush, Blood Trail?",
-    //4392: "Moonweaver in a Bush",
+	3549: "Spirit Elf Battalion (Night)",
+	3508: "Spirit Elf Battalion (Night)",
+	3592: "Spirit Elf Battalion (Night)",
+	4435: "Blood Trail towards Wolf Cave", //"Moonweaver in a Bush, Blood Trail?",
+	//4392: "Moonweaver in a Bush",
 	3761: "Subsided Formian Tunnel Under Attack",
-4138: "Perelline Guardians with Wilted Vines",
-3886: "Treant Arboretum, Destroyed by Wicker Circle",
-3929: "Perelline Guardian Destroyed by Wicker Circle",
-3802: "Wicker Circle Lumber Camp",
-    3931: "Mini Map",
+	4138: "Perelline Guardians with Wilted Vines",
+	3886: "Treant Arboretum, Destroyed by Wicker Circle",
+	3929: "Perelline Guardian Destroyed by Wicker Circle",
+	3802: "Wicker Circle Lumber Camp",
+	3931: "Mini Map",
 
-    4265: "Dead Man Climbing",
+	4265: "Dead Man Climbing",
 
-    3468: "Hangman Tree With A Secret (Day)",
+	3468: "Hangman Tree With A Secret (Day)",
 
-    3677: "Roc Carrying Angler Worm",
-    3718: "Roc Carrying Angler Worm",
-    3760: "Roc Carrying Angler Worm",
+	3677: "Roc Carrying Angler Worm",
+	3718: "Roc Carrying Angler Worm",
+	3760: "Roc Carrying Angler Worm",
 
-    3716: "Bog Iron Deposit",
-    3887: "Bog Iron Deposit",
-    3803: "Bog Iron Deposit",
+	3716: "Bog Iron Deposit",
+	3887: "Bog Iron Deposit",
+	3803: "Bog Iron Deposit",
 
-    3423: "Bank of Mist",
-    3381: "Bank of Mist",
-    3340: "Bank of Mist",
-    3298: "Bank of Mist",
+	3423: "Bank of Mist",
+	3381: "Bank of Mist",
+	3340: "Bank of Mist",
+	3298: "Bank of Mist",
 
-    3678: "Old Ruins with spare Stepsend",
+	3678: "Old Ruins with spare Stepsend",
 
-    3804: "Too Dense to Traverse",
+	3804: "Too Dense to Traverse",
 
-    4054: "Crimson Shambers",
-    3970: "Crimson Shambers Attacking Devils",
-    3928: "Crimson Shambers Near Giantish Obelisk",
+	4054: "Crimson Shambers",
+	3970: "Crimson Shambers Attacking Devils",
+	3928: "Crimson Shambers Near Giantish Obelisk",
 
-    4225: "Dropped Satchel of Missing Son",
+	4225: "Dropped Satchel of Missing Son",
 
-    4307: "Gith Trail",
+	4307: "Gith Trail",
 
-    4223: "Serpopard Den",
+	4223: "Serpopard Den",
 
-    4476: "Glademoor Bank of River",
+	4476: "Glademoor Bank of River",
 
-    4267: "Inhabited Farmhouse",
-3382: "Church of Rats Kuo-Toa Shrine",
-3469: "Wallowbank Wooden Watch Post",
-3470: "Peat Farm",
+	4267: "Inhabited Farmhouse",
+	3382: "Church of Rats Kuo-Toa Shrine",
+	3469: "Wallowbank Wooden Watch Post",
+	3470: "Peat Farm",
 
-    // === GLADEMOOR ===
-    4226: "Amber Blossoms",
-    4142: "Amber Blossoms",
-    4101: "Amber Blossoms",
-    4143: "Amber Blossoms",
-    4269: "Amber Blossoms",
-    4227: "Amber Blossoms",
+	// === GLADEMOOR ===
+	4226: "Amber Blossoms",
+	4142: "Amber Blossoms",
+	4101: "Amber Blossoms",
+	4143: "Amber Blossoms",
+	4269: "Amber Blossoms",
+	4227: "Amber Blossoms",
 
-    4478: "Bog Iron Deposits, Fort Nirunna Step-send",
+	4478: "Bog Iron Deposits, Fort Nirunna Step-send",
 
-    4310: "Old Ruins",
+	4310: "Old Ruins",
 
-    4351: "Aniel, Hamlet of Ghosts",
-    4309: "Fording the River to Glademoor",
-    3932: "Six-Knives camp", // Trying to hide from the wreckage of the Savage Dawn. They are sick and do not fight, but beg for their lives
+	4351: "Aniel, Hamlet of Ghosts",
+	4309: "Fording the River to Glademoor",
+	3932: "Six-Knives camp", // Trying to hide from the wreckage of the Savage Dawn. They are sick and do not fight, but beg for their lives
 
-    // === MÓDAR RUN
-    2841: "Incarnate Gloom (d8?)",
-    2925: "Incarnate Gloom (d8?)",
-    2756: "Incarnate Gloom (d8?)",
-    2882: "Incarnate Gloom (d8?)",
-    2798: "Incarnate Gloom (d8?)",
+	// === MÓDAR RUN
+	2841: "Incarnate Gloom (d8?)",
+	2925: "Incarnate Gloom (d8?)",
+	2756: "Incarnate Gloom (d8?)",
+	2882: "Incarnate Gloom (d8?)",
+	2798: "Incarnate Gloom (d8?)",
 
-    3175: "Lunarachinda Nest",
-3342: "Wallowbank Trading Caravan",
-3301: "The Tzénloch (the path you force yourself down)",
-3344: "Wallowbank Magical Waste Pool Defence Ranch",
-3343: "Wallowbank River Port",
+	3175: "Lunarachinda Nest",
+	3342: "Wallowbank Trading Caravan",
+	3301: "The Tzénloch (the path you force yourself down)",
+	3344: "Wallowbank Magical Waste Pool Defence Ranch",
+	3343: "Wallowbank River Port",
 
-    // === SARJORHN SLOPES ===
-    4516: "Broken Railway Track",
-    4263: "River Spring",
+	// === SARJORHN SLOPES ===
+	4516: "Broken Railway Track",
+	4263: "River Spring",
 
-    4052: "Stone Giant Believes He Is Dreaming",
-    4095: "Stone Giant Believes He Is Dreaming",
-    4011: "Stone Giant Believes He Is Dreaming",
+	4052: "Stone Giant Believes He Is Dreaming",
+	4095: "Stone Giant Believes He Is Dreaming",
+	4011: "Stone Giant Believes He Is Dreaming",
 
-    3672: "Hill Collapses Into Lake",
-    3546: "Hill Collapses Into Lake",
-    3630: "Hill Collapses Into Lake",
+	3672: "Hill Collapses Into Lake",
+	3546: "Hill Collapses Into Lake",
+	3630: "Hill Collapses Into Lake",
 
-    3631: "Hill Collapses Into Quag",
-    3547: "Hill Collapses Into Quag",
+	3631: "Hill Collapses Into Quag",
+	3547: "Hill Collapses Into Quag",
 
-    4180: "Dead Horses", // Shot with Gith arrows
+	4180: "Dead Horses", // Shot with Gith arrows
+	4222: "Druid's Grove",
+	4431: "Mine Surveyor Expedition", // Sonya Quinn is a wealthy mason from Meridian with Holdric Casterwell and Timothy Williams. She is looking to reopen the mine, and is quite an aggressive character. They have brought pickaxes, satches of drakedust (thermite), and prospecting gear to determine the value of reopening the former iron mine, currently inhabited b the moon weavers.
 
-    4431: "Mine Surveyor Expedition", // Sonya Quinn is a wealthy mason from Meridian with Holdric Casterwell and Timothy Williams. She is looking to reopen the mine, and is quite an aggressive character. They have brought pickaxes, satches of drakedust (thermite), and prospecting gear to determine the value of reopening the former iron mine, currently inhabited b the moon weavers.
+	// === FUMEPOOLS ===
+	3420: "Wyvern Spotting",
+	3378: "Wyvern Spotting",
+	3462: "Wyvern Spotting",
+	3504: "Wyvern Spotting",
 
-    // === FUMEPOOLS ===
-    3420: "Wyvern Spotting",
-    3378: "Wyvern Spotting",
-    3462: "Wyvern Spotting",
-    3504: "Wyvern Spotting",
+	// === CRAGRIVER PASS ===
 
-    // === CRAGRIVER PASS ===
-
-    3766: "Enthralled Six-Knives",
+	3766: "Enthralled Six-Knives",
 	2585: "Skirmish between Church of Rats and the Dredge",
-3303: "Peat Mammoth Farm",
-};
+	3303: "Peat Mammoth Farm"
+}
 
-export default autoencounters;
+export default autoencounters
